c7c531c8464f758e88bf3e61a46d1f42bc8fdfd3
Angelo Rapanà <angelorapana@icloud.com>
2026-04-13 11:19:23 +0200

    Creazione Middleware 404

--- express-blog-api-crud/app.js
+++ express-blog-api-crud/app.js
@@ -10,6 +10,13 @@ const postsRouter = require("./routes/posts");
 
 app.use("/posts", postsRouter);
 
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Errore 404",
+    message: "Non è stata trovata la risorsa richiesta"
+  });
+});
+
 app.listen(3000, () => {
   console.log("Prova");
 });
